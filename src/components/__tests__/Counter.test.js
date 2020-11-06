3f08603a7bd3bec2d0b5a40d4a56cb3938d86bc0
PeHaa <info@pehaa.com>
2020-11-06 17:10:10 +0100

    fix

--- src/components/__tests__/Counter.test.js
+++ src/components/__tests__/Counter.test.js
@@ -2,14 +2,30 @@ import React from "react"
 import { render, screen, fireEvents, fireEvent } from "@testing-library/react"
 import Counter from "../Counter"
 
-test("Component <Counter />", () => {
+/*
+Que testons nous ici ?
+Nous faisons abstraction (tant que c'est possible) de markup de notre components
+Si le developpeur change les tags de <p>Vous avez cliqué ... fois</p> pour
+<p>Vous avez cliqué <span>...</span> fois</p>, notre component continuera de fonctionner bien.
+Nous devrons pas être trop spécifique sur le "comment" la fonctionnalité est exactement implémenté.
+Nous testons la fonctionnalité elle même.
+
+Dans ce component, le "click" sur bouton devrait changer le texte "Vous avez cliqué ... fois"
+
+*/
+
+test("Component <Counter /> : counter starts at 0 and increments (+1) when the button is clicked", () => {
   render(<Counter />)
-  // container represent le #root element
+  // accès au bouton
   const button = screen.getByRole("button")
+  // acces à l'élément qui contient l'info sur le nombre de clicks
   const text = screen.getByText(/Vous avez cliqué/i)
+  // texte initial
   expect(text.textContent).toBe("Vous avez cliqué 0 fois.")
+  // click 1 fois
   fireEvent.click(button)
   expect(text.textContent).toBe("Vous avez cliqué 1 fois.")
+  // click 2 fois
   fireEvent.click(button)
   fireEvent.click(button)
   expect(text.textContent).toBe("Vous avez cliqué 3 fois.")
--- src/components/__tests__/CounterWithProps.test.js
+++ src/components/__tests__/CounterWithProps.test.js
@@ -2,9 +2,8 @@ import React from "react"
 import { render, screen, fireEvents, fireEvent } from "@testing-library/react"
 import CounterWithProps from "../CounterWithProps"
 
-test("Component <CounterWithProps />", () => {
+test("Component <CounterWithProps /> - counter starts at 0 and increments (+1) on click", () => {
   render(<CounterWithProps />)
-  // container represent le #root element
   const button = screen.getByRole("button")
   expect(button.textContent).toContain("+1")
   const text = screen.getByText(/Vous avez cliqué/i)
@@ -17,22 +16,16 @@ test("Component <CounterWithProps />", () => {
 })
 
 //initialCount = 2
-test("Component <CounterWithProps initialCount={2}/>", () => {
+test("Component <CounterWithProps initialCount={2} counter starts at {initialCount}/>", () => {
   render(<CounterWithProps initialCount={2} />)
-  // container represent le #root element
   const button = screen.getByRole("button")
   expect(button.textContent).toContain("+1")
-  const text = screen.getByText(/Vous avez cliqué/i)
-  expect(text.textContent).toBe("Vous avez cliqué 2 fois.")
-  fireEvent.click(button)
-  expect(text.textContent).toBe("Vous avez cliqué 3 fois.")
 })
 
 //initialCount = 2
 // increment = 3
-test("Component <CounterWithProps initialCount={2} increment={3}/>", () => {
+test("Component <CounterWithProps initialCount={2} increment={3}/> counter starts at {initialCount} and increments by {increment} on click", () => {
   render(<CounterWithProps initialCount={2} increment={3} />)
-  // container represent le #root element
   const button = screen.getByRole("button")
   expect(button.textContent).toContain("+3")
   const text = screen.getByText(/Vous avez cliqué/i)
